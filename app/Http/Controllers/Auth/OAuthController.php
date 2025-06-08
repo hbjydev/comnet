00b98936f24eacc2bc9a6c50fd29a07c6e98499e
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-06-08 19:27:09 +0100

    fix: move oauth routes to oauth controller

---
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Socialite\Facades\Socialite;
+
+class OAuthController extends Controller
+{
+    function discordRedirect() {
+        return Socialite::driver('discord')->redirect();
+    }
+
+    function discordCallback() {
+        $discordUser = Socialite::driver('discord')->user();
+        if ($user = User::where('discord_id', $discordUser->id)->first()) {
+            Auth::login($user);
+            return to_route('dashboard');
+        }
+
+        $url = $discordUser->avatar;
+        $contents = file_get_contents($url);
+        $avatarPath = 'user/avatar/' . $discordUser->id;
+        Storage::put($avatarPath, $contents);
+
+        $user = User::create([
+            'username' => $discordUser->name,
+            'display_name' => $discordUser->user['global_name'], // display name
+            'avatar' => $avatarPath,
+            'email' => $discordUser->email,
+            'email_verified_at' => now(),
+            'discord_id' => $discordUser->id,
+        ]);
+
+        Auth::login($user);
+
+        return to_route('dashboard');
+    }
+}
