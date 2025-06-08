de1540c6527b74f9e821485089fe62eb47da55c8
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-06-08 14:26:47 +0100

    fix: visual improvements

--- resources/js/pages/units/show.tsx
+++ resources/js/pages/units/show.tsx
@@ -7,7 +7,7 @@ import AppLayout from '@/layouts/app-layout';
 import { SharedData, Unit, type BreadcrumbItem } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
 import { AvatarFallback } from '@radix-ui/react-avatar';
-import { TableCellsMerge } from 'lucide-react';
+import { Cog, Columns3, ExternalLink, TableCellsMerge } from 'lucide-react';
 import moment from 'moment';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -45,19 +45,19 @@ export default function Show() {
 
             <div className="flex h-full flex-1 flex-col items-center gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="flex h-4 w-full max-w-screen-xl flex-col gap-4">
-                    <div className="flex items-center gap-x-4">
-                        <Avatar className="flex h-12 w-12 items-center justify-center bg-secondary">
-                            <AvatarImage src={unit.avatar} />
-                            <AvatarFallback>{initials(unit.display_name)}</AvatarFallback>
-                        </Avatar>
-                        <div className="space-y-0.5">
-                            <h2 className="text-xl font-semibold tracking-tight">{unit.display_name}</h2>
-                            <p className="text-sm text-muted-foreground">{`Created at ${unitCreated}`}</p>
-                        </div>
-                    </div>
+                    <div className="grid grid-cols-1 gap-4 xl:grid-cols-[auto_25rem] pt-2">
+                        <div className="space-y-4">
+                            <div className="flex items-center gap-x-4">
+                                <Avatar className="flex h-12 w-12 items-center justify-center bg-secondary">
+                                    <AvatarImage src={unit.avatar} />
+                                    <AvatarFallback>{initials(unit.display_name)}</AvatarFallback>
+                                </Avatar>
+                                <div className="space-y-0.5">
+                                    <h2 className="text-xl font-semibold tracking-tight">{unit.display_name}</h2>
+                                    <p className="text-sm text-muted-foreground">{`Created at ${unitCreated}`}</p>
+                                </div>
+                            </div>
 
-                    <div className="grid grid-cols-1 gap-4 xl:grid-cols-[auto_25rem]">
-                        <div className="space-y-4 pt-2">
                             <div className="flex flex-col gap-y-4">
                                 <HeadingSmall title="Description" />
 
@@ -65,12 +65,31 @@ export default function Show() {
                             </div>
                         </div>
                         <div className="space-y-4">
-                            <Button asChild>
-                                <Link href={route('units.orbat', { unit: unit.slug })}>
-                                    <TableCellsMerge />
-                                    ORBAT
-                                </Link>
-                            </Button>
+
+                            <div className="flex items-center gap-x-2">
+                                <Button asChild>
+                                    <Link href={route('units.orbat', { unit: unit.slug })}>
+                                        <TableCellsMerge />
+                                        ORBAT
+                                        <ExternalLink />
+                                    </Link>
+                                </Button>
+                                <Button asChild>
+                                    <Link href={route('units.orbat', { unit: unit.slug })}>
+                                        <Columns3 />
+                                        Structure
+                                        <ExternalLink />
+                                    </Link>
+                                </Button>
+                                <Button asChild>
+                                    <Link href={route('units.orbat', { unit: unit.slug })}>
+                                        <Cog />
+                                        Admin
+                                        <ExternalLink />
+                                    </Link>
+                                </Button>
+                            </div>
+
                             <MembersList unit={unit} />
                         </div>
                     </div>
