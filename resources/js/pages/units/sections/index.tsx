f44bb8bd5a736b65402e796f8cc647e6ef40bbcc
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-06-10 17:46:10 +0100

    feat: clean up, init sections page

---
+++ resources/js/pages/units/sections/index.tsx
@@ -0,0 +1,58 @@
+import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import AppLayout from '@/layouts/app-layout';
+import UnitStructureLayout from '@/layouts/units/structure';
+import { SharedData, Unit, UnitRank, type BreadcrumbItem } from '@/types';
+import { Head, usePage } from '@inertiajs/react';
+import {
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+    type ColumnDef,
+} from '@tanstack/react-table';
+import { Pencil, Trash } from 'lucide-react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Units',
+        href: '/units',
+    },
+];
+
+export default function Ranks() {
+    const { unit, ranks } = usePage<
+        SharedData & {
+            unit: Unit;
+            ranks: UnitRank[];
+        }
+    >().props;
+
+    return (
+        <AppLayout
+            breadcrumbs={[
+                ...breadcrumbs,
+                {
+                    title: unit.display_name,
+                    href: `/units/${unit.slug}`,
+                },
+                {
+                    title: 'Structure',
+                    href: `/units/${unit.slug}/ranks`,
+                },
+                {
+                    title: 'Sections',
+                    href: `/units/${unit.slug}/sections`,
+                },
+            ]}
+        >
+            <UnitStructureLayout>
+                <Head title="Sections" />
+                <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl">
+                </div>
+            </UnitStructureLayout>
+        </AppLayout>
+    );
+}
